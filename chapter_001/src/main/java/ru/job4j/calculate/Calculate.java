0cd0527486d09410a400bfb23721ab2717198332
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-10 14:40:11 +0300

    1.2 Подключить сборщик Maven[#84751

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,7 @@
+package ru.job4j.calculate;
+
+public class Calculate {
+    public String echo(String value) {
+        return String.format("Echo: %s", value);
+    }
+}
--- src/rub/job4/calculate/Calculate.java
+++
@@ -1,7 +0,0 @@
-package ru.jobj4.calculate;
-
-public class Calculate {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
