932d26aacc1be5b5adf7499f401d1d450f5aabb7
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-10 15:47:22 +0300

    1.3. JavaDoc. Code convention[#84752]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**class Calculate
+ * @author Vitali Baranov
+ * @version 1.0
+ *
+ */
 public class Calculate {
+    /**
+     * Method echo
+     * @author Vitali Baranov
+     * @param value string literal
+     * @return doubled value paramether
+     */
     public String echo(String value) {
         return String.format("Echo: %s", value);
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task
+ *
+ * @author Vitali Baranov (vitali.baranov@gmail.com)
+ * @version $ld$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
