50b70c834e06d21c51bce735ed1da68b5628e695
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-12 14:15:32 +0300

    5.3.0 Создать метод delete для односвязного списка[#84854]

---
+++ chapter_001/src/main/java/ru/job4j/baranov/list/SimpleLinkedList.java
@@ -0,0 +1,58 @@
+package ru.job4j.baranov.list;
+
+public class SimpleLinkedList<E> {
+
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E date) {
+        Node<E> newLink = new Node<>(date);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Реализовать метод удаления первого элемент в списке.
+     */
+    public E delete() {
+        E item = first.data;
+        first = first.next;
+        size--;
+        return item;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
