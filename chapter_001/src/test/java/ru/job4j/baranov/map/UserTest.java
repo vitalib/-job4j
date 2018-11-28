91422f1b215a486ebb37b662a6cf0cbb6bb4d2ba
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-28 11:41:41 +0300

    Add to map two users with default hashcode

--- chapter_001/src/test/java/ru/job4j/baranov/map/UserTest.java
+++ chapter_001/src/test/java/ru/job4j/baranov/map/UserTest.java
@@ -3,6 +3,8 @@ package ru.job4j.baranov.map;
 import org.junit.Test;
 
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -13,6 +15,23 @@ public class UserTest {
     public void whenInvokeUserConstructorUserObjectIsCreated(){
         User user = new User("name", 0, new GregorianCalendar(1983, 11, 24));
         assertThat(user instanceof User, is(true));
+    }
+
+    @Test
+    public void whenTwoUsersWithSameFieldsWithouProperHashAndEqualsAreStoredTwiceInMap(){
+        User user1 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
+        User user2 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, user1.getName());
+        map.put(user2, user2.getName());
+        System.out.println(map);
+        System.out.println("user1 hashcode = " + user1.hashCode());
+        System.out.println("user2 hashcode = " + user2.hashCode());
+
+        assertEquals(map.size(), 2);
+        assertNotEquals(user1.hashCode(), user2.hashCode());
+
     }
 
 }
