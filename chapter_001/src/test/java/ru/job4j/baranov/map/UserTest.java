f8d8a849c96cb8f8f84d72238ab9cced1b650b3a
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-28 12:04:27 +0300

    Define equals func

--- chapter_001/src/test/java/ru/job4j/baranov/map/UserTest.java
+++ chapter_001/src/test/java/ru/job4j/baranov/map/UserTest.java
@@ -37,6 +37,7 @@ public class UserTest {
     }
 
     @Test
+    @Ignore
     public void whenTwoUsersWithSameFieldsWithProperHashwithouEqualsAreStoredTwiceInMap(){
         User user1 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
         User user2 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
@@ -51,5 +52,23 @@ public class UserTest {
         assertEquals(map.size(), 2);
         assertEquals(user1.hashCode(), user2.hashCode());
     }
+
+    @Test
+    public void whenTwoUsersWithSameFieldsWithoutProperHashwithProperEqualsAreStoredTwiceInMap(){
+        User user1 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
+        User user2 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, user1.getName());
+        map.put(user2, user2.getName());
+        System.out.println(map);
+        System.out.println("user1 hashcode = " + user1.hashCode());
+        System.out.println("user2 hashcode = " + user2.hashCode());
+
+        assertEquals(map.size(), 2);
+        assertNotEquals(user1.hashCode(), user2.hashCode());
+        assertTrue(user1.equals(user2));
+    }
+
 
 }
