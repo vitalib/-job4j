561c7a960b80b3d692e66277e3aeb7ec370f1ad0
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-28 11:57:35 +0300

    Add hashCode() to User

--- chapter_001/src/main/java/ru/job4j/baranov/map/User.java
+++ chapter_001/src/main/java/ru/job4j/baranov/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.baranov.map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -35,5 +36,10 @@ public class User {
 
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
