90df5a7cdd16ad4a72b4e2a2a86e437c0ad38254
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-28 12:10:41 +0300

    Add hashCode and equals

--- chapter_001/src/main/java/ru/job4j/baranov/map/User.java
+++ chapter_001/src/main/java/ru/job4j/baranov/map/User.java
@@ -48,4 +48,8 @@ public class User {
                 Objects.equals(birthday, user.birthday);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
 }
--- chapter_001/src/test/java/ru/job4j/baranov/map/UserTest.java
+++ chapter_001/src/test/java/ru/job4j/baranov/map/UserTest.java
@@ -54,6 +54,7 @@ public class UserTest {
     }
 
     @Test
+    @Ignore
     public void whenTwoUsersWithSameFieldsWithoutProperHashwithProperEqualsAreStoredTwiceInMap(){
         User user1 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
         User user2 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
@@ -70,5 +71,22 @@ public class UserTest {
         assertTrue(user1.equals(user2));
     }
 
+
+    @Test
+    public void whenTwoUsersWithSameFieldsWithProperHashandEqualsAreStoredOnceInMap(){
+        User user1 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
+        User user2 = new User("Vitali", 0, new GregorianCalendar(1983, 11, 24));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, user1.getName());
+        map.put(user2, user2.getName());
+        System.out.println(map);
+        System.out.println("user1 hashcode = " + user1.hashCode());
+        System.out.println("user2 hashcode = " + user2.hashCode());
+
+        assertEquals(map.size(), 1);
+        assertEquals(user1.hashCode(), user2.hashCode());
+        assertTrue(user1.equals(user2));
+    }
 
 }
