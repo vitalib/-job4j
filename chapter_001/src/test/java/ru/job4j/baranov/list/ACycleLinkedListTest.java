946eb3fc2979987e0a04bf619b264bff5339014f
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-28 10:19:31 +0300

    Modify test, delete fields

--- chapter_001/src/test/java/ru/job4j/baranov/list/ACycleLinkedListTest.java
+++ chapter_001/src/test/java/ru/job4j/baranov/list/ACycleLinkedListTest.java
@@ -9,15 +9,13 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class ACycleLinkedListTest {
-    private int size;
-    private ACycleLinkedList list;
-    private ArrayList<Node<Integer>> nodeArrayList;
 
-    @Before
-    public void setUp() {
-        size = 10;
-        list = new ACycleLinkedList();
-        nodeArrayList = new ArrayList<>();
+
+    @Test
+    public void whenNoCycleInLinkedListReturnFalse() {
+        int size = 10;
+        ACycleLinkedList list = new ACycleLinkedList();
+        ArrayList<Node<Integer>> nodeArrayList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             nodeArrayList.add(new Node(i));
         }
@@ -26,17 +24,23 @@ public class ACycleLinkedListTest {
             nodeArrayList.get(i).next = nodeArrayList.get(i + 1);
         }
 
-    }
-
-    @Test
-    public void whenNoCycleInLinkedListReturnFalse() {
-
         assertThat(list.hasCycle(nodeArrayList.get(0)), is(false));
 
     }
 
     @Test
     public void whenCycleInLinkedListReturnTrue() {
+        int size = 10;
+        ACycleLinkedList list = new ACycleLinkedList();
+        ArrayList<Node<Integer>> nodeArrayList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            nodeArrayList.add(new Node(i));
+        }
+
+        for (int i = size - 2; i >= 0; i--) {
+            nodeArrayList.get(i).next = nodeArrayList.get(i + 1);
+        }
+
 
         nodeArrayList.get(size - 1).next = nodeArrayList.get(size / 2);
         assertThat(list.hasCycle(nodeArrayList.get(0)), is(true));
