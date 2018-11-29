8b25b5a852d3f9ca3f0727ed3f27ce2999581a99
Vitali Baranov <vitali.baranov@gmail.com>
2018-11-29 12:51:13 +0300

    Generic HashMap with iterator

---
+++ chapter_001/src/main/java/ru/job4j/baranov/map/MyHashMap.java
@@ -0,0 +1,132 @@
+package ru.job4j.baranov.map;
+
+import java.util.Iterator;
+
+public class MyHashMap <K, V> implements Iterable<K> {
+
+    private Object[] array = new Object[16];
+    double loadFactor = 0.75;
+    int elements = 0;
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<K>() {
+            int index = 0;
+            Node tmp = null;
+
+            @Override
+            public boolean hasNext() {
+                if (tmp != null) {
+                    return true;
+                }
+                while (index < array.length && (tmp = (Node) array[index++]) == null) {
+                }
+                return tmp != null;
+            }
+
+            @Override
+            public K next() {
+                if (hasNext()) {
+                    K result = tmp.key;
+                    tmp = tmp.next;
+                    return result;
+                }
+                return null;
+            }
+        };
+    }
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node next;
+
+        public Node (K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public boolean insert(K key, V value){
+        boolean result = true;
+        int bucket = Math.abs(key.hashCode()) % array.length;
+        Node tmp;
+        for (tmp = (Node) array[bucket]; tmp != null; tmp = tmp.next) {
+            if (key.hashCode() == tmp.key.hashCode() && key.equals(tmp.key)) {
+                if (value.equals(tmp.value)) {
+                    result = false;
+                    break;
+                } else {
+                    tmp.value = value;
+                    break;
+                }
+            }
+        }
+        if (tmp == null) {
+            array[bucket] = new Node(key, value, (Node) array[bucket]);
+            elements++;
+        }
+        if (elements / array.length > loadFactor) {
+            resize();
+        }
+        return result;
+    }
+
+    public V get(K key) {
+        int bucket = Math.abs(key.hashCode()) % array.length;
+        if (array[bucket] != null) {
+            for (Node tmp = (Node) array[bucket]; tmp != null; tmp = tmp.next) {
+                if (tmp.key.equals(key)) {
+                    return tmp.value;
+                }
+            }
+        }
+        return null;
+    }
+
+    private void resize() {
+        int arraySize = array.length;
+        Object[] tmp = new Object[arraySize];
+        System.arraycopy(array, 0, tmp, 0, array.length);
+        array = new Object[arraySize * 2];
+        elements = 0;
+        for (int i = 0; i < arraySize; i++) {
+            for (Node tmpNode = (Node) tmp[i]; tmpNode != null; tmpNode = tmpNode.next) {
+                insert(tmpNode.key, tmpNode.value);
+            }
+        }
+    }
+
+
+    public int getSize() {
+        return elements;
+    }
+
+
+    boolean delete (K key) {
+        boolean result = false;
+        int bucket = Math.abs(key.hashCode()) % array.length;
+        if (array[bucket] != null) {
+            Node first = (Node) array[bucket];
+            if (first.key.equals(key)){
+                array[bucket] = first.next;
+                elements--;
+            } else {
+               for (Node tmp = (Node) array[bucket]; tmp.next != null; tmp = tmp.next) {
+                   if (tmp.next.key.equals(key)) {
+                      result = true;
+                      tmp.next = tmp.next.next;
+                      elements--;
+                      break;
+                   }
+               }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MyHashMap map = new MyHashMap();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/baranov/map/MyHashMapTest.java
@@ -0,0 +1,87 @@
+package ru.job4j.baranov.map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyHashMapTest {
+
+    @Test
+    public void whenInsertOnePairSizeEqualsOne() {
+        MyHashMap<Integer, Integer> map = new MyHashMap();
+        map.insert(5, 5);
+        assertEquals(1, map.getSize());
+    }
+
+    @Test
+    public void whenInsertOnePairGetSameValueBack() {
+        MyHashMap<Integer, Integer> map = new MyHashMap();
+        map.insert(5, 5);
+        assertEquals(new Integer(5), map.get(5));
+    }
+
+    @Test
+    public void whenInsertOnePairThenDeleteItThenSizeEqualNull() {
+        MyHashMap<Integer, Integer> map = new MyHashMap();
+        map.insert(5, 5);
+        map.delete(5);
+        assertEquals(0, map.getSize());
+    }
+
+
+    @Test
+    public void whenInsertThirtySizeIsThirty (){
+        MyHashMap<Integer, Integer> map = new MyHashMap<>();
+
+        for (int i = 0; i < 30; i++ ) {
+            map.insert(i, i);
+        }
+        assertEquals(30, map.getSize());
+    }
+
+
+    @Test
+    public void whenInsertTwoValueCanIterateOnThem() {
+        MyHashMap<Integer, Integer> map = new MyHashMap<>();
+        boolean[] test = {false, false};
+        map.insert(0, 0);
+        map.insert(1, 1);
+
+        int count = 0;
+        for (Integer key: map) {
+            test[map.get(key)] = true;
+            count++;
+        }
+        assertEquals(2, count);
+        assertTrue(test[0]);
+        assertTrue(test[1]);
+
+    }
+
+    @Test
+    public void whenInsertManyValuesAndDeleteAllOfThemSizeEqualsZero() {
+        MyHashMap<Integer, Integer> map = new MyHashMap<>();
+        int n = 1000000;
+        for (int i = 0; i < n; i++) {
+            map.insert(i, i);
+        }
+        assertEquals(n, map.getSize());
+        for (int i = 0; i < n; i++) {
+            map.delete(i);
+        }
+        assertEquals(0, map.getSize());
+
+    }
+
+    @Test
+    public void whenInsertSameKeyValueTwiceSizeIsOne() {
+        MyHashMap<String, String> map = new MyHashMap<>();
+
+        map.insert("vitali", "baranov");
+        map.insert("vitali", "baranov");
+
+        assertEquals(1, map.getSize());
+    }
+
+
+}
